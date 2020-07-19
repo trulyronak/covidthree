0703ce62a919dbc2bf5bb5c8d0957052bc906d8a
Ronak Shah <ronakshahultimate@gmail.com>
2020-07-18 20:19:58 -0700

    init

---
+++ precache-manifest.a525ee3d4e1e5df13b4bfae174149014.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ffd134e28c5430887e3114417f2815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01ef0e6f39cc800639d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2e94307fc586d85f9b04",
+    "url": "/static/css/main.be7e0306.chunk.css"
+  },
+  {
+    "revision": "a01ef0e6f39cc800639d",
+    "url": "/static/js/2.97bc5c6f.chunk.js"
+  },
+  {
+    "revision": "4369a283cb702f6e1b40c254f3619ee6",
+    "url": "/static/js/2.97bc5c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e94307fc586d85f9b04",
+    "url": "/static/js/main.09d6f5fb.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
